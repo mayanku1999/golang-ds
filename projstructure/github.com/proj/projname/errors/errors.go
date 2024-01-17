048d5d86f5682146131848b3abb76a186149704e
Mayank Upadhyay <37012441+mayanku1999@users.noreply.github.com>
2024-01-18 00:45:14 +0530

    add enum value

--- projstructure/github.com/proj/projname/errors/errors.go
+++ projstructure/github.com/proj/projname/errors/errors.go
@@ -3,3 +3,14 @@ package errors
 import "fmt"
 
 var RecordNotFound = fmt.Errorf("Record not found")
+
+
+
+type TaskStatusType string
+
+const (
+	Created   TaskStatusType = "Created"
+	Pending   TaskStatusType = "Pending"
+	SpillOver TaskStatusType = "SpillOver"
+	Completed TaskStatusType = "Completed"
+)
